2f9e094e49d5fec745bb95d3be51b0867bb20dfd
Murat AKMAMEDAU <muratx10@gmail.com>
2019-08-10 12:01:30 +0300

    feat: init firebase db

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,23 @@ window.addEventListener('DOMContentLoaded', () => {
     const submit = document.querySelector('.btn');
     const userList = document.querySelector('.userList ol');
     const allUsers = [];
-
+    
+    //Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyDYcP91akP7ognhjGLNKYsbbFWZ9S4etmM",
+        authDomain: "test-task-geeksolutions.firebaseapp.com",
+        databaseURL: "https://test-task-geeksolutions.firebaseio.com",
+        projectId: "test-task-geeksolutions",
+        storageBucket: "",
+        messagingSenderId: "1059729712279",
+        appId: "1:1059729712279:web:f87f35a7f092b008"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+    //firebase database reference
+    const db = firebase.database().ref();
+    
     const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
     //password must contain minimum 8 chars, at least one letter & one number
@@ -48,6 +64,9 @@ window.addEventListener('DOMContentLoaded', () => {
         userList.innerHTML += html;
     };
 
+    //user id in database
+    let id = 1;
+
     submit.addEventListener('click', e => {
         e.preventDefault();
         let emailValid = email.classList.value;
@@ -56,17 +75,28 @@ window.addEventListener('DOMContentLoaded', () => {
         //     return item.email === email.value
         // });
         if (emailValid.includes('is-valid') && email.value.length < 40 && passwordValid.includes('is-valid')) {
-            let obj = {
-                'email': email.value,
-                'password': password.value
-            };
-            allUsers.push(obj);
-            console.log(allUsers);
+            // let obj = {
+            //     'email': email.value,
+            //     'password': password.value
+            // };
+            // allUsers.push(obj);
+            
+            // db.child(`User ${id}`).child('e-mail').set(email.value);
+            // db.child(`User ${id}`).child('passkey').set(password.value);
+            // id++;
+            db.child(`User ${id}`).set({
+                'E-mail': email.value,
+                'Password': password.value
+            });
+            id++;
+
             injectHTML(email.value);
             loginForm.reset();
             email.classList.remove('is-valid');
             password.classList.remove('is-valid');
         } else return false;
+
+
     });
 
 
